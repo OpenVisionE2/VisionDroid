65d91897481ef880412027abfc060efd4410ccf4
Persian Prince <persianpros@yahoo.com>
2022-07-10 08:58:15 +0430

    Update apk.sh

--- CI/apk.sh
+++ CI/apk.sh
@@ -4,10 +4,9 @@ VNAME=`cat app/build.gradle | grep "versionName=" | cut -d "=" -f2 | sed 's|"||g
 
 mkdir -p apk
 rm -f apk/*.apk
-mv -f app/build/outputs/apk/google/debug/app-google-universal-debug.apk apk/VisionDroid-${$VNAME}.apk
+mv -f app/build/outputs/apk/google/debug/app-google-universal-debug.apk apk/VisionDroid-${VNAME}.apk
 rm -rf app/build
 
 git add -u
 git add *
-git commit -m "Publish latest apk file"
-
+git commit -m "Publish latest ${VNAME} apk file"

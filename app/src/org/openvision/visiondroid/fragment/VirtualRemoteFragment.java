9bbbdb11d275e988eb1aaf51dd920566ceeb6e84
AYP <persianpros@yahoo.com>
2023-05-04 10:40:34 +0330

    Update VirtualRemoteFragment.java

--- app/src/org/openvision/visiondroid/fragment/VirtualRemoteFragment.java
+++ app/src/org/openvision/visiondroid/fragment/VirtualRemoteFragment.java
@@ -12,17 +12,16 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Vibrator;
 import android.preference.PreferenceManager;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import org.openvision.visiondroid.VisionDroid;
 import org.openvision.visiondroid.R;
@@ -39,9 +38,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * A Virtual STB remote control using http-requests to send key-strokes
+ * A Virtual dreambox remote control using http-requests to send key-strokes
  *
- * @author original creator
+ * @author sreichholf
  */
 public class VirtualRemoteFragment extends BaseHttpFragment {
     @NonNull
@@ -122,10 +121,14 @@ public class VirtualRemoteFragment extends BaseHttpFragment {
         buttonMap.put(R.id.ButtonTv, Remote.KEY_TV);
         buttonMap.put(R.id.ButtonRadio, Remote.KEY_RADIO);
         buttonMap.put(R.id.ButtonText, Remote.KEY_TEXT);
-        return buttonMap.entrySet()
-                .stream()
-                .map(e -> new Integer[]{e.getKey(), e.getValue()})
-                .toArray(Integer[][]::new);
+
+        Integer[][] keys = new Integer[buttonMap.size()][];
+        int i = 0;
+        for (Map.Entry<Integer,Integer> entry: buttonMap.entrySet()) {
+            keys[i] = new Integer[]{entry.getKey(), entry.getValue()};
+            i++;
+        }
+        return keys;
     }
 
     @Override
@@ -285,7 +288,99 @@ public class VirtualRemoteFragment extends BaseHttpFragment {
             hasError = true;
             toastText = stateText;
         }
-
+//package org.openvision.visiondroid.helpers.enigma2.epgsync;
+//
+//import android.app.NotificationManager;
+//import android.content.Context;
+//import android.graphics.Bitmap;
+//import android.graphics.BitmapFactory;
+//
+//import androidx.annotation.NonNull;
+//import androidx.core.app.NotificationCompat;
+//import android.util.Log;
+//
+//import org.openvision.visiondroid.DatabaseHelper;
+//import org.openvision.visiondroid.VisionDroid;
+//import org.openvision.visiondroid.R;
+//import org.openvision.visiondroid.helpers.ExtendedHashMap;
+//import org.openvision.visiondroid.helpers.NameValuePair;
+//import org.openvision.visiondroid.helpers.SimpleHttpClient;
+//import org.openvision.visiondroid.helpers.enigma2.Event;
+//import org.openvision.visiondroid.helpers.enigma2.Service;
+//import org.openvision.visiondroid.helpers.enigma2.requesthandler.EventListRequestHandler;
+//import org.openvision.visiondroid.helpers.enigma2.requesthandler.ServiceListRequestHandler;
+//
+//import java.util.ArrayList;
+//
+///**
+// * Created by Stephan on 18.04.2014.
+// */
+//public class EpgDatabase {
+//    public static final String TAG_EPG_DATABASE = EpgDatabase.class.getSimpleName();
+//
+//    private NotificationManager mNotifyManager;
+//    private NotificationCompat.Builder mBuilder;
+//    int mId = 1;
+//    DatabaseHelper mDbh;
+//
+//    public void syncBouquet(@NonNull Context context, String reference){
+//        mNotifyManager =
+//                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+//        mBuilder = new NotificationCompat.Builder(context, "visiondroid_picon_sync");
+//
+//        Bitmap bm = BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher);
+//        mBuilder.setContentTitle(context.getString(R.string.epg_sync))
+//                .setLargeIcon(bm)
+//                .setSmallIcon(R.drawable.ic_action_refresh);
+//
+//        mDbh = DatabaseHelper.getInstance(context);
+//
+//        SimpleHttpClient shc = SimpleHttpClient.getInstance(VisionDroid.getCurrentProfile());
+//		ServiceListRequestHandler slh = new ServiceListRequestHandler();
+//		ArrayList<NameValuePair> args = new ArrayList<>();
+//		args.add(new NameValuePair("sRef", reference));
+//
+//		String xml = slh.getList(shc, args);
+//		ArrayList<ExtendedHashMap> services = new ArrayList<>();
+//		if(xml != null){
+//			slh.parseList(xml, services);
+//            Log.i(TAG_EPG_DATABASE, String.format("Syncing EPG for Bouquet %s with %s services", reference, services.size()));
+//
+//            int size = services.size();
+//            int cnt = 0;
+//			for(ExtendedHashMap service : services){
+//                mBuilder.setContentText(service.getString(Event.KEY_SERVICE_NAME)).setProgress(size, cnt, false).setOngoing(true);
+//                mNotifyManager.notify(mId, mBuilder.build());
+//				syncService(context, service.getString(Service.KEY_REFERENCE));
+//                cnt++;
+//			}
+//            mBuilder.setContentText(context.getString(R.string.epg_sync_finished)).setProgress(0, 0, false).setOngoing(false);
+//            mNotifyManager.notify(mId, mBuilder.build());
+//		}
+//	}
+//
+//	public void syncService(Context context, String reference){
+//		SimpleHttpClient shc = SimpleHttpClient.getInstance(VisionDroid.getCurrentProfile());
+//		EventListRequestHandler elh = new EventListRequestHandler();
+//		ArrayList<NameValuePair> args = new ArrayList<>();
+//		args.add(new NameValuePair("sRef", reference));
+//
+//		String xml = elh.getList(shc, args);
+//		ArrayList<ExtendedHashMap> events = new ArrayList<>();
+//
+//        int success = 0;
+//
+//		if(xml != null){
+//			elh.parseList(xml, events);
+//			success += setEvents(events);
+//		}
+//        Log.i(TAG_EPG_DATABASE, String.format("Synchronized %s events", success));
+//    }
+//
+//	public int setEvents(@NonNull ArrayList<ExtendedHashMap> events){
+//        return mDbh.setEvents(events);
+//	}
+//}
         if (hasError) {
             showToast(toastText);
         } else {
